1b92594215a56892bffc2aa67b7e7f6c405143b1
Vyacheslav Maron <vmaronmail@gmail.com>
2021-12-24 17:23:39 -0500

    Add Tables and Buttons pages

--- src/app/modules/ui-features/ui-features-routing.module.ts
+++ src/app/modules/ui-features/ui-features-routing.module.ts
@@ -2,9 +2,13 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {NotFoundComponent} from './pages/not-found/not-found.component';
 import {BlankComponent} from './pages/blank/blank.component';
+import {TablesComponent} from './pages/tables/tables.component';
+import {ButtonsComponent} from './pages/buttons/buttons.component';
 
 const routes: Routes = [
   {path: '', component: NotFoundComponent},
+  {path: 'tables', component: TablesComponent},
+  {path: 'buttons', component: ButtonsComponent},
   {path: 'blank', component: BlankComponent},
   {path: '404', component: NotFoundComponent}
 ];
--- src/app/modules/ui-features/ui-features.module.ts
+++ src/app/modules/ui-features/ui-features.module.ts
@@ -4,10 +4,16 @@ import {CommonModule} from '@angular/common';
 import {UiFeaturesRoutingModule} from './ui-features-routing.module';
 import {NotFoundComponent} from './pages/not-found/not-found.component';
 import {BlankComponent} from './pages/blank/blank.component';
+import {ButtonsComponent} from './pages/buttons/buttons.component';
+import {TablesComponent} from './pages/tables/tables.component';
 
 
 @NgModule({
-  declarations: [NotFoundComponent, BlankComponent],
+  declarations: [
+    NotFoundComponent,
+    BlankComponent,
+    TablesComponent,
+    ButtonsComponent],
   imports: [
     CommonModule,
     UiFeaturesRoutingModule
