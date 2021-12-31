3b0e5c22c69a59f4290585f557d859afc0351313
Vyacheslav Maron <vmaronmail@gmail.com>
2021-12-31 12:17:01 -0500

    Add body and footer projections for the shared dialog component

--- src/app/modules/dialogs/components/modal/modal.component.ts
+++ src/app/modules/dialogs/components/modal/modal.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, Input} from '@angular/core';
 import {DialogsService} from '../../dialogs.service';
 
 
@@ -9,6 +9,7 @@ import {DialogsService} from '../../dialogs.service';
 })
 export class ModalComponent<T> {
   display = true;
+  @Input() title: string;
 
   constructor(private dialogsService: DialogsService<T>) {
   }
