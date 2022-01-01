d1f2a43eb3d497072375da0dec6a3a79ff9bb45f
Vyacheslav Maron <vmaronmail@gmail.com>
2022-01-01 16:36:05 -0500

    Add animations to global styles

--- src/app/modules/dialogs/components/drawer/drawer.component.ts
+++ src/app/modules/dialogs/components/drawer/drawer.component.ts
@@ -1,47 +1,28 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {DialogsService} from '../../dialogs.service';
-import {animate, state, style, transition, trigger} from '@angular/animations';
 
 @Component({
   selector: 'app-drawer',
   templateUrl: './drawer.component.html',
   styleUrls: ['./drawer.component.scss'],
-  animations: [
-    trigger('slideInOut', [
-      state('in', style({
-        transform: 'translate3d(0,0,0)'
-      })),
-      state('out', style({
-        transform: 'translate3d(100%, 0, 0)'
-      })),
-      transition('in => out', animate('500ms ease-in-out')),
-      transition('out => in', animate('500ms ease-in-out'))
-    ]),
-  ]
 })
 export class DrawerComponent<T> implements OnInit {
-  drawerState = 'out';
-  display = true;
+  drawerState = 'slide-out';
   @Input() title: string;
 
   constructor(private dialogsService: DialogsService<T>) {
   }
 
   ngOnInit(): void {
-    setTimeout(async () => {
-      this.toggle();
-    });
+    setTimeout(async () => this.toggle());
   }
 
   toggle() {
-    this.drawerState = this.drawerState === 'out' ? 'in' : 'out';
+    this.drawerState = this.drawerState === 'slide-out' ? 'slide-in' : 'slide-out';
   }
 
   async close(): Promise<void> {
     this.toggle();
-    setTimeout(async () => {
-      this.display = false;
-      await this.dialogsService.close();
-    }, 750);
+    setTimeout(async () => await this.dialogsService.close(), 750);
   }
 }
